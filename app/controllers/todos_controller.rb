7688f4d1ef756963f4e7e6539550c1bbb2fbe7cb
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2024-05-13 10:46:44 +0700

    Update todos_controller to pass back the id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -46,6 +46,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.require(:todo).permit(:content, :due_date, :priority, :completed)
+      params.require(:todo).permit(:id, :content, :due_date, :priority, :completed)
     end
 end
